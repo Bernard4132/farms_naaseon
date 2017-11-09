c67c4cee8f0737049c2b296e915643128affddcc
Bernard Lomo <bernardlomo2@gmail.comm>
2017-11-09 10:51:29 +0000

    new edits

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,8 @@
 class OrderMailer < ApplicationMailer
-	default from: "order@naaseonfarms.com"
+	default from: "NaaseonFarms@naaseonfarms.com"
 
   def order_fulfillment_alert(order)
   	@order = order
-  	mail to: order.buyeremail , subject: "Your Order has been fulfilled"
+  	mail to: order.buyeremail , subject: "[Farm Produce Order] Your Order has been fulfilled."
   end
 end
